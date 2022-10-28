4b167def3b52ba64ff6a2cca234fd1f1801473a8
Ilvondir <komsa.m@o2.pl>
2022-10-28 12:24:00 +0200

    social media added

--- js/email.js
+++
@@ -1,7 +0,0 @@
-$("#subscribe").on("click", function subscribe() {
-    let address = $("#email").val();
-
-    if (address != "") {
-        alert("Adres " + address + " został zapisany na subskrypcję!")
-    }
-});
