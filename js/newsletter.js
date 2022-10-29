848be1888d3d9738f2bde96e22d700aaa39caf15
Ilvondir <komsa.m@o2.pl>
2022-10-29 17:34:05 +0200

    un/checking all added

--- js/newsletter.js
+++ js/newsletter.js
@@ -19,10 +19,28 @@ function main(xml) {
 
     let tab = xmlData.getElementsByTagName("subscriber");
     for (let i = 0; i <tab.length; i++) {
-        document.querySelector("table").innerHTML += "<tr><td><input type=\"checkbox\" id=\"person" + (i+1) + "\" class=\"subscribers\"></td><td><label for=\"subscriber" + (i+1) + "\">" + tab[i].getElementsByTagName("name")[0].childNodes[0].nodeValue + "<br>" + tab[i].getElementsByTagName("email")[0].childNodes[0].nodeValue + "</label></td></tr>";   
+        document.querySelector("table").innerHTML += "<tr><td><input type=\"checkbox\" id=\"person" + (i+1) + "\" class=\"subscribers\"></td><td><label for=\"person" + (i+1) + "\">" + tab[i].getElementsByTagName("name")[0].childNodes[0].nodeValue + "<br>" + tab[i].getElementsByTagName("email")[0].childNodes[0].nodeValue + "</label></td></tr>";
     }
 }
 
 function checkAllSubscribers() {
-    console.log("Działam!");
+    let toCheck = document.querySelectorAll(".subscribers");
+    
+    for (let i=0; i<toCheck.length; i++) {
+        toCheck[i].checked = true;
+    }
+
+    document.getElementById("checkAll").setAttribute("onchange", "uncheckAllSubscribers()");
+    document.querySelector("label[for=\"checkAll\"]").textContent = "Odznacz wszystkich";
+}
+
+function uncheckAllSubscribers() {
+    let toCheck = document.querySelectorAll(".subscribers");
+    
+    for (let i=0; i<toCheck.length; i++) {
+        toCheck[i].checked = false;
+    }
+
+    document.getElementById("checkAll").setAttribute("onchange", "checkAllSubscribers()");
+    document.querySelector("label[for=\"checkAll\"]").textContent = "Zaznacz wszystkich";
 }
